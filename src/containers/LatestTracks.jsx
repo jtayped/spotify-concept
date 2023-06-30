d183f77dd7c9c501a1d26c5eb41ece1637894f25
Joel Taylor <jtayped@gmail.com>
2023-06-30 12:18:00 +0200

    latest playlists fetch

--- src/containers/LatestTracks.jsx
+++ src/containers/LatestTracks.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const LatestTracks = ({spotify}) => {
+const PlaylistPreview = ({ playlistData }) => {
+  return <div></div>;
+};
+
+const LatestTracks = ({ spotify }) => {
+  const [playlists, setPlaylists] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    // Fetch recently played tracks
+    spotify.getMyRecentlyPlayedTracks({ limit: 50 }).then((response) => {
+      const recentlyPlayedTracks = response.items;
+      const playlistIds = recentlyPlayedTracks
+        .filter((track) => track.context && track.context.type === "playlist")
+        .map((track) => track.context.uri.split(":")[2]);
+
+      // Remove duplicate playlist ids
+      const uniquePlaylistIds = [...new Set(playlistIds)];
+
+      // Fetch playlist data for each unique id
+      const fetchPlaylistData = async () => {
+        const playlistDataPromises = uniquePlaylistIds.map((playlistId) =>
+          spotify.getPlaylist(playlistId)
+        );
+        const playlistData = await Promise.all(playlistDataPromises);
+        setPlaylists(playlistData);
+      };
+
+      fetchPlaylistData();
+      setLoading(false);
+    });
+  }, [spotify]);
+
+  if (loading) {
+    return <div></div>;
+  }
   return (
-    <div>LatestTracks</div>
-  )
-}
+    <ul className="grid grid-cols-3">
+      {playlists.map((playlists, index) => (
+        <li key={index}>{playlists.name}</li>
+      ))}
+    </ul>
+  );
+};
 
-export default LatestTracks
+export default LatestTracks;
