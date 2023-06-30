28e00cbecdeb1f22aabb777460feb7da69ac6c45
Joel Taylor <jtayped@gmail.com>
2023-06-30 12:39:42 +0200

    latest track first design

--- src/containers/LatestTracks.jsx
+++ src/containers/LatestTracks.jsx
@@ -1,7 +1,37 @@
+// React Util
 import React, { useEffect, useState } from "react";
 
+// Icons
+import { BsPlayFill } from "react-icons/bs";
+
 const PlaylistPreview = ({ playlistData }) => {
-  return <div></div>;
+  console.log(playlistData);
+  return (
+    <div className="p-3 xl:p-2 bg-white/10 rounded-xl flex flex-col xl:flex-row gap-3 justify-between">
+      <div className="flex flex-col xl:flex-row gap-2 xl:gap-5 overflow-hidden">
+        <a href={playlistData.external_urls.spotify}>
+          <img
+            className="w-full aspect-square xl:max-h-[100px] xl:min-w-[100px] rounded"
+            src={playlistData.images[0].url}
+            alt=""
+          />
+        </a>
+        <div className="flex flex-col text-xs max-w-[270px]">
+          <h3 className="font-bold text-xl text-text whitespace-nowrap truncate ...">
+            {playlistData.name}
+          </h3>
+          <p className="line-clamp-2 xl:line-clamp-4">
+            {playlistData.description}
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <button className="rounded-full xl:rounded bg-primary-button p-2">
+          <BsPlayFill size={18} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 const LatestTracks = ({ spotify }) => {
@@ -39,9 +69,11 @@ const LatestTracks = ({ spotify }) => {
     return <div></div>;
   }
   return (
-    <ul className="grid grid-cols-3">
-      {playlists.map((playlists, index) => (
-        <li key={index}>{playlists.name}</li>
+    <ul className="grid grid-cols-3 gap-3">
+      {playlists.map((playlistData, index) => (
+        <li key={index}>
+          <PlaylistPreview playlistData={playlistData} />
+        </li>
       ))}
     </ul>
   );
