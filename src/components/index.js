fd0dd910836bc089f66c86ba86270b0612019f8d
Joel Taylor <jtayped@gmail.com>
2023-07-01 11:46:04 +0200

    component created

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as SpotifyAuth } from "./SpotifyAuth";
 export { default as AlbumPreviewSm } from "./AlbumPreviewSm";
 export { default as TrackPreviewSm } from "./TrackPreviewSm";
 export { default as ArtistPreviewSm } from "./ArtistPreviewSm";
+export { default as PlaylistPreview } from "./PlaylistPreview";
--- src/containers/LatestTracks.jsx
+++ src/containers/LatestTracks.jsx
@@ -1,37 +1,8 @@
 // React Util
 import React, { useEffect, useState } from "react";
 
-// Icons
-import { BsPlayFill } from "react-icons/bs";
-
-const PlaylistPreview = ({ playlistData }) => {
-  return (
-    <div className="p-3 xl:p-2 bg-white/10 rounded-xl flex flex-col xl:flex-row gap-3 justify-between border border-gray-50/10">
-      <div className="flex flex-col xl:flex-row gap-2 xl:gap-5 overflow-hidden">
-        <a href={playlistData.external_urls.spotify}>
-          <img
-            className="w-full aspect-square xl:max-h-[100px] xl:min-w-[100px] rounded-md"
-            src={playlistData.images[0].url}
-            alt=""
-          />
-        </a>
-        <div className="flex flex-col text-xs max-w-[270px]">
-          <h3 className="font-bold text-xl text-text whitespace-nowrap truncate ...">
-            {playlistData.name}
-          </h3>
-          <p className="line-clamp-2 xl:line-clamp-4">
-            {playlistData.description}
-          </p>
-        </div>
-      </div>
-      <div className="flex gap-2">
-        <button className="rounded-full xl:rounded bg-primary-button hover:bg-primary-button/90 p-2">
-          <BsPlayFill size={18} />
-        </button>
-      </div>
-    </div>
-  );
-};
+// JSX Components
+import { PlaylistPreview } from "../components";
 
 const LatestTracks = ({ spotify }) => {
   const [playlists, setPlaylists] = useState([]);
