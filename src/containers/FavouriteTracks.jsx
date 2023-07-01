dd1048179141fafb5752c77aa648308747701bf2
Joel Taylor <jtayped@gmail.com>
2023-07-01 12:57:01 +0200

    class purging

--- src/containers/FavouriteTracks.jsx
+++ src/containers/FavouriteTracks.jsx
@@ -6,24 +6,42 @@ import { TrackPreviewMd } from "../components";
 
 const FavouriteTracks = ({ spotify }) => {
   const [tracks, setTracks] = useState(null);
+  const [numTracks, setNumTracks] = useState(6);
 
   useEffect(() => {
     spotify
-      .getMyTopTracks({ limit: 6 })
+      .getMyTopTracks()
       .then((data) => setTracks(data))
       .catch((err) => {
         console.error(err);
         return <div></div>;
       });
-  }, [spotify, tracks]);
+  }, [spotify]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setNumTracks(2); // Adjust the number of tracks for smaller widths
+      } else if (window.innerWidth < 1024) {
+        setNumTracks(4); // Adjust the number of tracks for medium widths
+      } else {
+        setNumTracks(6); // Default number of tracks for larger widths
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call the function on initial load
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   if (!tracks) {
     return <div></div>;
   }
 
   return (
-    <div className="grid grid-cols-6 gap-3 z-[11]">
-      {tracks.items.map((track, index) => (
+    <div className={`grid grid-cols-[${numTracks}] gap-3 z-[11]`}>
+      {tracks.items.slice(0, numTracks).map((track, index) => (
         <TrackPreviewMd key={index} trackData={track} />
       ))}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 sidebarWidth = "300px";
 module.exports = {
+  purge: {
+    options: {
+      safelist: [/^grid-cols-/],
+    },
+  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
