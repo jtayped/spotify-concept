e970d06ea031002672dcdd1af637a5423df85066
Joel Taylor <jtayped@gmail.com>
2023-07-01 12:14:14 +0200

    artists added

--- src/components/TrackPreviewMd.jsx
+++ src/components/TrackPreviewMd.jsx
@@ -18,9 +18,9 @@ const TrackPreviewMd = ({ trackData }) => {
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className="flex flex-col justify-between bg-white/10 hover:bg-white/20 border border-gray-50/10 rounded-lg p-2"
+      className="flex flex-col bg-white/10 hover:bg-white/20 border border-gray-50/10 rounded-lg p-2"
     >
-      <div className="flex flex-col relative">
+      <div className="relative">
         <img src={trackData.album.images[0].url} alt="Track" />
         <button
           className={`absolute right-1 bottom-1 rounded-full bg-primary-button hover:bg-primary-button/90 p-2 ${
@@ -30,8 +30,24 @@ const TrackPreviewMd = ({ trackData }) => {
           <BsPlayFill size={18} />
         </button>
       </div>
-
       <h3 className="line-clamp-2 text-lg">{trackData.name}</h3>
+      <ol className="text-xs gap-1 w-[200px] md:w-[300px]">
+        {trackData.artists.map((artist, index) => (
+          <React.Fragment key={artist.id}>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={artist.external_urls.spotify}
+                className="hover:underline"
+              >
+                {artist.name}
+              </a>
+              {index !== trackData.artists.length - 1 && ", "}
+            </li>
+          </React.Fragment>
+        ))}
+      </ol>
     </div>
   );
 };
