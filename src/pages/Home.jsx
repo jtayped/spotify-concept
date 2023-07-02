88a5042209e3768afe3826c414fe8fce923be4ed
Joel Taylor <jtayped@gmail.com>
2023-07-02 19:30:37 +0200

    background image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const spotify = new SpotifyWebApi();
 
 let _spotifyToken = "";
 const Home = () => {
-  const [currentlyPlaying, setCurrentlyPlaying] = useState({});
+  const [backgroundImage, setBackgroundImage] = useState("");
 
   const [hasToken, setHasToken] = useState(false);
   const navigate = useNavigate();
@@ -36,14 +36,18 @@ const Home = () => {
     return <div></div>;
   }
   return (
-    <div className="text-text bg-background font-gotham overflow-hidden">
+    <div className="relative text-text bg-background font-gotham">
       <SideBar spotify={spotify} />
-      <main className="md:ml-sidebar p-5 md:p-10 flex flex-col gap-5 h-full">
+      <main className="relative md:ml-sidebar p-5 md:p-10 flex flex-col gap-5 h-full">
         <LatestPlaylists spotify={spotify} />
         <FavouriteTracks spotify={spotify} />
         <PortfolioBanner />
+        <img
+          className="absolute top-0 left-0 w-full h-screen opacity-30 blur-lg object-cover object-center"
+          src="https://i.scdn.co/image/ab67616d0000b273750d9fe0a5648ad5fde3a44e"
+          alt="Background"
+        />
       </main>
-      <div className="absolute z-[0] w-[60%] h-[60%] top-0 green__gradient" />
     </div>
   );
 };
