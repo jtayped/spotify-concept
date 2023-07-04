9e737c0cc10dc1a9ec41eb277ecda009c9f60ece
Joel Taylor <jtayped@gmail.com>
2023-07-04 13:51:18 +0200

    name change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 // JSX Components
-import App from "./App1";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
